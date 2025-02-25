9243d9be757d7b9468cf18adb38d75f47e46e78a
João Renato Mendes <joao.mendes@mindminers.com>
2025-02-25 19:25:12 -0300

    refactor(raw_to_bronze): adjusted TRANSFORMATIONS.get()

--- src/lib/ingestors.py
+++ src/lib/ingestors.py
@@ -1,54 +1,37 @@
-from delta.tables import DeltaTable
-from pyspark.sql.window import Window
-from pyspark.sql.functions import row_number, col
+import sys
+sys.path.append(f'/Workspace/Users/{dbutils.widgets.get("account")}/music_data_lake/src/lib')
+
 import utils
+from preprocessing import TRANSFORMATIONS, transform_generic
+from ingestors import GenericIngestor
+
+# Pega o JSON passado como argumento do job
+table_config = {
+  "catalog": dbutils.widgets.get("catalog"),
+  "schema": dbutils.widgets.get("schema"),
+  "tablename": dbutils.widgets.get("tablename"),
+  "primary_key": dbutils.widgets.get("primary_key"),
+  "timestamp_field": dbutils.widgets.get("timestamp_field")
+}
+
+ingestor = GenericIngestor(spark, table_config)
+
+# Full Load, se necessário
+ingestor.execute_full_load()
+
+# Lê o stream
+table_schema = utils.import_schema(ingestor.tablename)
+df_stream = (spark.readStream
+                  .format("parquet")
+                  .option("cloudFiles.format", "parquet")
+                  .schema(table_schema)
+                  .load(f"/Volumes/raw/{ingestor.schema}/cdc/postgres.public.{ingestor.tablename}/"))
 
 
-class GenericIngestor:
-    def __init__(self, spark, config):
-        self.spark = spark
-        self.config = config
-        self.catalog = config["catalog"]
-        self.schema = config["schema"]
-        self.tablename = config["tablename"]
-        self.primary_key = config["primary_key"]
-        self.timestamp_field = config["timestamp_field"]
-        self.table_path = f"{self.catalog}.{self.schema}.{self.tablename}"
+# Aplica transformação específica se existir, caso contrario aplica transformação genérica
+transform_function = TRANSFORMATIONS.get(ingestor.schema, "others")
+df_stream = transform_function(df_stream)
 
-    def execute_full_load(self):
-        if not utils.table_exists(self.spark, catalog=self.catalog, schema=self.schema, table=self.tablename):
-            df_full = self.spark.read.format("parquet").load(f'/Volumes/raw/{self.schema}/full_load/{self.tablename}/')
-            df_full.coalesce(1).write.format("delta").mode("overwrite").saveAsTable(self.table_path)
-
-    def upsert(self, df, batch_id):
-        deltatable = DeltaTable.forName(self.spark, self.table_path)
-
-        df_filtered = df.filter(f"{self.primary_key} IS NOT NULL")
-        windowSpec = Window.partitionBy(self.primary_key).orderBy(col(self.timestamp_field).desc())
-
-        df_cdc = (
-            df_filtered.withColumn("row_number", row_number().over(windowSpec))
-            .filter(col("row_number") == 1)
-            .drop("row_number")
-        )
-
-        (
-            deltatable.alias("target")
-            .merge(df_cdc.alias("source"), f"target.{self.primary_key} = source.{self.primary_key}")
-            .whenMatchedDelete(condition="source.operation = 'd'")
-            .whenMatchedUpdateAll(condition="source.operation = 'u'")
-            .whenNotMatchedInsertAll(condition="source.operation = 'c' OR source.operation = 'u'")
-            .execute()
-        )
-
-    def process_stream(self, df_stream):
-        """Agora a transformação ocorre antes de chamar esta função."""
-        stream = (
-            df_stream.writeStream.option(
-                "checkpointLocation",
-                f"/Volumes/raw/{self.schema}/cdc/postgres.public.{self.tablename}/{self.tablename}_checkpoint/",
-            )
-            .foreachBatch(lambda df, batch_id: self.upsert(df, batch_id))
-            .trigger(availableNow=True)
-        )
-        return stream
+# Processa o CDC
+cdc_stream = ingestor.process_stream(df_stream)
+cdc_stream.start()
